11ede6e350c5c0e0817f40596353c8048b6b92cc
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-14 14:05:27 +0000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -33,6 +33,14 @@ function Product(id, name, price, expiryDate) {
 
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {};
+const dateDiff = (date1, date2) => {
+    let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    return diffDays;
+};
+const dateDiff = (date1, date2) => {};
 
 // Here, use Object.defineProperty to create property - daysToExpire
 
