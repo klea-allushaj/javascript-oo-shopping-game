0cfd51c4194b4672a655613eed15959715525dcb
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-14 13:34:34 +0000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -16,7 +16,14 @@ let player = {
     deductPoints(points) {
         this.score = this.score - points;
     }
-};// Define the Product class - write the Constructor function for Product class here
+};
+// Define the Product class - write the Constructor function for Product class here
+function Product(id, name, price, expiryDate) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.expiryDate = expiryDate;
+}
 
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {};
