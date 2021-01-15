a458c06966be03c2ce510c8ed388b1a457332d51
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-15 08:47:30 +0000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -231,7 +231,7 @@ const findProductById = (id) => {
 
 // Complete this function
 const generateProductId = () => {
-  return Math.random() * (20) + 1;
+  return Math.floor(Math.random() * (20) + 1);
 };
 
 
